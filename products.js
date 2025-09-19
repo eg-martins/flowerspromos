ed049ba3a256308cb53379f223f3ad31e6a2098f
flowerspromos <ed.world.martins@gmail.com>
2025-09-19 12:18:24 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,21 @@
+// Lista de produtos
+const productsData = [
+  {
+    id: "P001",
+    img: "https://i.imgur.com/XfuJFf9.png",
+    title: "Escova Secadora 3 em 1 — Seca, Alisa e Modela com Brilho de Salão",
+    desc: "Diga adeus à rotina demorada com secador, prancha e modelador! A Escova Secadora Elétrica 3 em 1 combina tudo em um só aparelho, permitindo secar, alisar e modelar os fios em poucos minutos. Com tecnologia de íons negativos, elimina o frizz, protege a umidade natural e deixa o cabelo macio, sedoso e cheio de brilho. Potência de 1000W para resultados rápidos e eficazes. Ideal para todos os tipos de cabelo!",
+    platform: "Shopee",
+    category: "beleza",
+    link: "https://s.shopee.com.br/70AqHmYs5l"
+  },
+  {
+    id: "P002",
+    img: "https://i.imgur.com/xf49VdO.png",
+    title: "Máquina Automática Para Descascar Alho e Frutas — Praticidade Instantânea",
+    desc: "Chega de perder tempo descascando alho, tomates, uvas e outras frutas pequenas! Com a Máquina Automática de Descascamento, basta apertar um botão e o trabalho pesado é feito em segundos. Perfeita para uso doméstico ou comercial, ela descasca de forma rápida e delicada, preservando a polpa dos alimentos e trazendo muito mais agilidade para sua cozinha.",
+    platform: "Shopee",
+    category: "casa",
+    link: "https://s.shopee.com.br/50Pn6vyS3c"
+  },
+];
