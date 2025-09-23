7ec34fe6ee6cea7a23149e7f29f681ae6a0f24c9
flowerspromos <ed.world.martins@gmail.com>
2025-09-23 21:20:20 +0100

    Grande atualizacao parte 4 de 4

--- products.js
+++ products.js
@@ -1,12 +1,13 @@
 // Lista de produtos
 const productsData = [
-  {
+{
     id: "P001",
     img: "https://i.imgur.com/XfuJFf9.png",
     title: "Escova Secadora 3 em 1 — Seca, Alisa e Modela com Brilho de Salão",
     desc: "Diga adeus à rotina demorada com secador, prancha e modelador! A Escova Secadora Elétrica 3 em 1 combina tudo em um só aparelho, permitindo secar, alisar e modelar os fios em poucos minutos. Com tecnologia de íons negativos, elimina o frizz, protege a umidade natural e deixa o cabelo macio, sedoso e cheio de brilho. Potência de 1000W para resultados rápidos e eficazes. Ideal para todos os tipos de cabelo!",
     platform: "Shopee",
-    category: "beleza",
+    category: ["beleza"], 
+    subcategory: ["cabelo"],
     link: "https://s.shopee.com.br/70AqHmYs5l"
   },
   {
@@ -15,7 +16,8 @@ const productsData = [
     title: "Máquina Automática Para Descascar Alho e Frutas — Praticidade Instantânea",
     desc: "Chega de perder tempo descascando alho, tomates, uvas e outras frutas pequenas! Com a Máquina Automática de Descascamento, basta apertar um botão e o trabalho pesado é feito em segundos. Perfeita para uso doméstico ou comercial, ela descasca de forma rápida e delicada, preservando a polpa dos alimentos e trazendo muito mais agilidade para sua cozinha.",
     platform: "Shopee",
-    category: "casa",
+    category: ["casa"], 
+    subcategory: ["cozinha"], 
     link: "https://s.shopee.com.br/50Pn6vyS3c"
   },
   {
@@ -24,7 +26,8 @@ const productsData = [
     title: "Suporte de Parede Multifuncional para Vassouras e Rodos — Organização Fácil",
     desc: "Mantenha sua casa organizada e seus utensílios de limpeza sempre à mão com o Suporte Multifuncional de Parede. Compatível com vassouras, rodos, panos e outros itens, ele economiza espaço e protege seus objetos do desgaste. Fabricado em plástico resistente, suporta múltiplos itens e é ideal para cozinhas, banheiros ou áreas de serviço. Instalação simples e segura, mantendo tudo sempre no lugar!",
     platform: "Shopee",
-    category: "casa",
+    category: ["casa"],
+    subcategory: ["organizacao"],
     link: "https://s.shopee.com.br/1BD4jlnm42"
   },
   {
@@ -33,16 +36,17 @@ const productsData = [
     title: "Protetor de Calcanhar Adesivo – Conforto e Proteção para Seus Sapatos",
     desc: "Diga adeus ao desconforto causado por sapatos apertados! O Protetor de Calcanhar Adesivo oferece acolchoamento macio e proteção eficaz contra fricção, prevenindo bolhas e irritações. Fácil de aplicar e remover, discreto e compatível com todos os tipos de calçados, garante conforto prolongado durante o uso de tênis, botas ou saltos altos. Ideal para quem passa longas horas calçando sapatos e deseja um ajuste perfeito!",
     platform: "Shopee",
-    category: "acessorios",
+    category: ["acessorios"],
     link: "https://s.shopee.com.br/3qDpwQvBjN"
   },
-   {
+  {
     id: "P005",
     img: "https://i.imgur.com/ERHmTFa.png",
     title: "Aparador Retrô 2 Prateleiras — Cantinho do Café Elegante e Funcional",
     desc: "Deixe seu cantinho do café organizado e com estilo com o Aparador Retrô de 2 Prateleiras. Fabricado em MDF de alta qualidade, com acabamento branco e mogno, ele combina beleza e funcionalidade. Montagem prática e rápida, sem necessidade de ferramentas. Ideal para organizar bebidas, utensílios ou objetos decorativos, tornando seu espaço mais elegante e funcional.",
     platform: "Shopee",
-    category: "casa",
+    category: ["casa"],
+    subcategory: ["decoracao"],
     link: "https://s.shopee.com.br/7Kni94AWPp"
   },
 ];
